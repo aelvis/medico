78fc8894e814ad5d56965eb33194c6f44b7c2a4e
Elvis Alcántara Pinedo <aelvis@users.noreply.github.com>
2019-03-22 19:19:31 -0500

    actualizar
    actualizar

--- src/app/component/consulta/consulta.component.ts
+++ src/app/component/consulta/consulta.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-consulta',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consulta.component.css']
 })
 export class ConsultaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+	public atencion:any = [];
+  constructor(private toastr: ToastrService, private _usuarioService: UsuarioService, private _router: Router) { }
+	ngOnInit(){
+		this.obtenerCitas();
+	}
+	showSuccess(titulo,mensaje) {
+    	this.toastr.success(mensaje, titulo);
+  	}
+  	showError(titulo,mensaje) {
+    	this.toastr.error(mensaje, titulo);
+  	}
+	obtenerCitas(){
+		this._usuarioService.obtenerCitasMedicoSucursal().subscribe(
+			res => {
+				if(res["mensaje"].terminar){
+				  	localStorage.clear();
+				  	this._router.navigate(['/login']);
+				}else{
+					if(res["mensaje"].medico){
+						this.atencion = res["mensaje"].medico;
+					}else{
+						this.atencion = "No hay productos...";
+					}
+				}
+			},
+			error => {
+				this.showSuccess("Alerta","Error de Internet");
+			}
+		);
+	}
 
 }
